5f132c7178fbde5f85509ac801763579b2faaf68
Haadi Razzak <haadirazzak@gmail.com>
2024-09-13 21:25:14 -0700

    Small fix added for spacing

--- src/components/DocumentCard.tsx
+++ src/components/DocumentCard.tsx
@@ -8,7 +8,7 @@ interface DocumentCardProps {
 
 export default function DocumentCard({ title, timestamp, onClick }: DocumentCardProps) {
   return (
-    <div onClick={onClick} className="bg-white p-4 rounded-lg shadow w-[280px] sm:w-[300px] md:w-[320px] border border-[#CACACA] cursor-pointer">
+    <div onClick={onClick} className="bg-white p-4 rounded-lg shadow w-[280px] sm:w-[300px] md:w-[320px] border border-[#CACACA] cursor-pointer ml-8">
       <div className="bg-[#D9D9D9] h-40 w-auto mt-2 mx-2 mb-2 flex items-center justify-center rounded">
         <Image src="/Document.svg" alt="Document Icon" width={50} height={40} />
       </div>
